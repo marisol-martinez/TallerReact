1e79b68cccf9d1de6348576e6e8213dddbf087b6
marisol-martinez <71198601+marisol-martinez@users.noreply.github.com>
2021-02-16 09:08:15 -0300

    Se actualiza app.js

--- entregaGit/src/App.js
+++ entregaGit/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 //import './estilos.css';
 import Login from './Componentes/Login';
 import Registro from './Componentes/Registro';
+import Menu from './Componentes/Menu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Login/> */}
-        <Registro/>
+        <Login/>
+        {/* <Registro/> */}
+        {/* <Menu/> */}
       </header>
     </div>
   );
