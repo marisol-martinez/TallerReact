2848831ef9aa8f74981e1d213562ca1c7448e297
marisol-martinez <71198601+marisol-martinez@users.noreply.github.com>
2021-02-24 13:04:47 -0300

    actualizo app.js

--- entregaGit/src/App.js
+++ entregaGit/src/App.js
@@ -5,21 +5,47 @@ import Login from './Componentes/Login';
 import Registro from './Componentes/Registro';
 import Menu from './Componentes/Menu';
 import Dashboard from './Componentes/Dashboard';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+let initialState = {
+  nombreAplicacion: "Tareas",
+  idTareaActual: 3,
+  tareas: [
+    {id:1, nombre:"Tarea1"},
+    {id:2, nombre:"Tarea2"}
+  ]
+}
+
+let reducer = (state = initialState, action) => {
+  console.log(state,action);
+  switch(action.type){
+    case "PRUEBA":
+      return {...state, idTareaActual: state.idTareaActual +1};
+    case "AgregarEntrenamiento":
+      return state;
+    case "EliminarEntrenamiento":
+      return state;
+    default: 
+      return state;
+  }
+  
+}
+
+let store = createStore(reducer);
+
+store.dispatch({type: "PRUEBA"});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <body>
-        {/* <Login/> */}
-        {/* <Registro/> */}
-        {/* <Menu/> */}
-        <Dashboard/>
-
-      </body>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+          {/* <Login/> */}
+          <Registro/>
+          {/* <Menu/> */}
+          {/* <Dashboard/> */}
+      </div>
+    </Provider>
   );
 }
 
